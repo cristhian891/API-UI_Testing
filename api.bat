542f3083cc3f5ca8b16f948e4290623617671974
Cristhian Preciado <cristhian891@users.noreply.github.com>
2019-02-15 17:29:09 +0000

    Update api.bat

--- api.bat
+++ api.bat
@@ -1,7 +1,7 @@
 #!/bin/sh
 set Python C:\Users\cpreciado\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Python 3.6
 cd c:\Users\cpreciado\PycharmProjects\ComplyAdvantage\UI_Tests\Tests\Functional_Tests
-pytest
+python -m pytest
 
 
 
