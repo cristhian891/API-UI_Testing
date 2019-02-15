60561233a67ca320a63c21fd736a0b3fb78a0326
Cristhian Preciado <cristhian891@users.noreply.github.com>
2019-02-15 17:20:52 +0000

    Update api.bat

--- api.bat
+++ api.bat
@@ -2,4 +2,4 @@
 cd c:\Users\cpreciado\PycharmProjects\ComplyAdvantage\UI_Tests\Tests\Functional_Tests
 pytest
 
-PYTHONPATH=C:\Users\cpreciado\PycharmProjects\ComplyAdvantage\venv\Scripts\python.exe
+C:\Users\cpreciado\Python\Python36\python.exe
