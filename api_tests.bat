e61c0d86757644ba5bec4231de6003433d9a030c
Cristhian Preciado <cristhian891@users.noreply.github.com>
2019-06-08 16:32:06 +0100

    t

--- api_tests.bat
+++ api_tests.bat
@@ -1,6 +1,7 @@
 @ECHO OFF
 setlocal
-set PYTHONPATH=C:\Users\crist\AppData\Local\Programs\Python\Python37
+set PYTHONPATH="C:\Users\crist\AppData\Local\Programs\Python\Python37"
+endlocal
 cd api_tests
 cd Tests
 python -m pytest
