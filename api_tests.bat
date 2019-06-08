38a2d30f5fdec09e07e6e222f40673bd645873b5
Cristhian Preciado <cristhian891@users.noreply.github.com>
2019-06-08 15:57:32 +0100

    changes

--- api_tests.bat
+++ api_tests.bat
@@ -1,4 +1,4 @@
 #!/bin/sh
-set Python C:\Users\crist\AppData\Local\Programs\Python\Python37
+set python C:\Users\crist\AppData\Local\Programs\Python\Python37
 cd C:\Users\crist\PycharmProjects\API-UI_Testing-master\api_tests\Tests
 python -m pytest
